788c029b1ec8a4ffb0822cb71e70bc35bd48b7aa
Liam Arzola <liam0215@gmail.com>
2022-04-05 13:48:56 -0400

    change parseDIDToken to not rely on atob

--- src/utils/parse-didt.ts
+++ src/utils/parse-didt.ts
@@ -1,3 +1,4 @@
+import { toByteArray } from 'base64-js';
 import { Claim, ParsedDIDToken } from '../types';
 import { isDIDTClaim } from './type-guards';
 import { createMalformedTokenError } from '../core/sdk-exceptions';
@@ -7,12 +8,23 @@ interface ParseDIDTokenResult {
   withParsedClaim: ParsedDIDToken;
 }
 
+function convertUint8ArrayToBinaryString(u8Array: Uint8Array) {
+  let i;
+  const len = u8Array.length;
+  let b_str = '';
+  for (i = 0; i < len; i++) {
+    b_str += String.fromCharCode(u8Array[i]);
+  }
+  return b_str;
+}
+
 /**
  * Parses a DID Token so that the encoded `claim` is in object form.
  */
 export function parseDIDToken(DIDToken: string): ParseDIDTokenResult {
   try {
-    const [proof, claim] = JSON.parse(globalThis.atob(DIDToken)) as [string, string];
+    const did_binary_string = convertUint8ArrayToBinaryString(toByteArray(DIDToken));
+    const [proof, claim] = JSON.parse(did_binary_string) as [string, string];
     const parsedClaim = JSON.parse(claim) as Claim;
     if (isDIDTClaim(parsedClaim)) return { raw: [proof, claim], withParsedClaim: [proof, parsedClaim] };
     throw new Error();
